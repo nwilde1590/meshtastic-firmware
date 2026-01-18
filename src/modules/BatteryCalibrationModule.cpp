9ab6733e8b6f63c006978d9247c45fcbe6ffe8c0
noahwilde <47438354+noahwilde@users.noreply.github.com>
2026-01-17 18:58:03 -0600

    Simplifying frame

--- src/modules/BatteryCalibrationModule.cpp
+++ src/modules/BatteryCalibrationModule.cpp
@@ -1,55 +1,28 @@
-#include "configuration.h"
+#include "BatteryCalibrationModule.h"
+#include "graphics/SharedUIDisplay.h"
+#include "graphics/ScreenFonts.h"
+
+BatteryCalibrationModule::BatteryCalibrationModule()
+    : SinglePortModule("battery-calibration", meshtastic_PortNum_PRIVATE_APP)
+{
+}
+
+ProcessMessage BatteryCalibrationModule::handleReceived(const meshtastic_MeshPacket &mp)
+{
+    (void)mp;
+    return ProcessMessage::CONTINUE;
+}
 
 #if HAS_SCREEN
-
-#include "BatteryCalibrationModule.h"
-#include "graphics/ScreenFonts.h"
-#include "graphics/SharedUIDisplay.h"
-#include "main.h"
-#include "mesh/generated/meshtastic/module_config.pb.h"
-
-BatteryCalibrationModule::BatteryCalibrationModule() : MeshModule("BatteryCalibration")
+void BatteryCalibrationModule::drawFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y)
 {
-    if (inputBroker) {
-        inputObserver.observe(inputBroker);
-    }
-}
-
-int BatteryCalibrationModule::handleInputEvent(const InputEvent *event)
-{
-    if (event->inputEvent == static_cast<char>(meshtastic_ModuleConfig_CannedMessageConfig_InputEventChar_BACK) ||
-        event->inputEvent == static_cast<char>(meshtastic_ModuleConfig_CannedMessageConfig_InputEventChar_CANCEL)) {
-        if (screen) {
-            screen->showPrevFrame();
-        }
-    }
-    return 0;
-}
-
-void BatteryCalibrationModule::drawFrame(OLEDDisplay *display, OLEDDisplayUiState *, int16_t x, int16_t y)
-{
-    if (!screen)
-        return;
+    (void)state;
     display->clear();
     display->setTextAlignment(TEXT_ALIGN_LEFT);
     display->setFont(FONT_SMALL);
-    int line = 1;
 
-    // === Set Title
     const char *titleStr = "Battery Calibration";
 
-    // === Header ===
     graphics::drawCommonHeader(display, x, y, titleStr);
-
-    if (powerStatus->getHasBattery()) {
-        char voltageStr[24];
-        const int batV = powerStatus->getBatteryVoltageMv() / 1000;
-        const int batCv = (powerStatus->getBatteryVoltageMv() % 1000) / 10;
-        snprintf(voltageStr, sizeof(voltageStr), "Voltage: %01d.%02dV", batV, batCv);
-        display->drawString(x, graphics::getTextPositions(display)[line++], voltageStr);
-    } else {
-        display->drawString(x, graphics::getTextPositions(display)[line++], "Voltage: USB");
-    }
 }
-
 #endif
--- src/modules/BatteryCalibrationModule.h
+++ src/modules/BatteryCalibrationModule.h
@@ -1,28 +1,17 @@
 #pragma once
 
-#include "configuration.h"
+#include "SinglePortModule.h"
 
-#if HAS_SCREEN
-
-#include "Observer.h"
-#include "input/InputBroker.h"
-#include "mesh/MeshModule.h"
-
-class BatteryCalibrationModule : public MeshModule
+class BatteryCalibrationModule : public SinglePortModule
 {
   public:
     BatteryCalibrationModule();
 
-    virtual bool wantUIFrame() override { return true; }
-    virtual void drawFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y) override;
+#if HAS_SCREEN
+    bool wantUIFrame() override { return true; }
+    void drawFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y) override;
+#endif
 
   protected:
-    virtual bool wantPacket(const meshtastic_MeshPacket *p) override { return false; }
-
-  private:
-    int handleInputEvent(const InputEvent *event);
-    CallbackObserver<BatteryCalibrationModule, const InputEvent *> inputObserver =
-        CallbackObserver<BatteryCalibrationModule, const InputEvent *>(this, &BatteryCalibrationModule::handleInputEvent);
+    ProcessMessage handleReceived(const meshtastic_MeshPacket &mp) override;
 };
-
-#endif
